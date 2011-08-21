fee4d9e1d6b331c1500fd912ecabb631dac04ca4
dotnetjunky <none@none>
2011-08-21 16:29:30 -0700

    Add content files to NuGetPackageExplorer.Types package.

---
+++ Packages/content/MyCustomPackageCommand.cs.pp
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuGet;
+using NuGetPackageExplorer.Types;
+
+namespace $rootnamespace$ 
+{
+
+    // TODO: replace 'My custom command' with your menu label
+    [PackageCommandMetadata("My custom command")]
+    internal class MyCustomPackageCommand : IPackageCommand 
+    {
+        public void Execute(IPackage package) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Packages/content/MyCustomPackageRule.cs.pp
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NuGetPackageExplorer.Types;
+using System.ComponentModel.Composition;
+using NuGet;
+
+namespace $rootnamespace$ 
+{
+
+    [Export(typeof(IPackageRule))]
+    internal class MyCustomPackageRule : IPackageRule 
+    {
+
+        public string Name 
+        {
+            get 
+            {
+                return "My Custom Rule";
+            }
+        }
+
+        public IEnumerable<PackageIssue> Check(IPackage package) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
