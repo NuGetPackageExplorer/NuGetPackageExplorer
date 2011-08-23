6f21ac30a3b39647053cc55c0ef7aa8100bdf6b3
dotnetjunky <none@none>
2011-08-22 20:29:58 -0700

    add menu to show/hide Common Tasks pane.
    --HG--
    branch : 2.0

--- PackageExplorer/Converters/AndLogicConverter.cs
+++ PackageExplorer/Converters/AndLogicConverter.cs
@@ -9,7 +9,17 @@ namespace PackageExplorer {
             if (values.Any(v => v == null || v == DependencyProperty.UnsetValue)) {
                 return false;
             }
-            return values.Cast<bool>().All(a => a);
+
+            if (targetType == typeof(bool)) {
+                return values.Cast<bool>().All(a => a);
+            }
+            else if (targetType == typeof(Visibility)) {
+                bool notVisible = values.Cast<Visibility>().Any(v => v != Visibility.Visible);
+                return notVisible ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else {
+                return null;
+            }
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) {
