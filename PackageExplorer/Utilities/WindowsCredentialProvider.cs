ea7d75fda62e9723b56327addcd399d31b544e0c
dotnetjunky <none@none>
2011-09-24 08:07:25 -0700

    Delete commented code.

--- PackageExplorer/Utilities/WindowsCredentialProvider.cs
+++ PackageExplorer/Utilities/WindowsCredentialProvider.cs
@@ -13,36 +13,6 @@ namespace PackageExplorer {
             }
 
             return PromptUserForCredentials(uri, forcePrompt: true);
-
-            //// Capture the original proxy before we do anything 
-            //// so that we can re-set it once we get the credentials for the given Uri.
-            //IWebProxy originalProxy = null;
-            //if (proxy != null) {
-            //    // If the current Uri should be bypassed then don't try to get the specific
-            //    // proxy but simply capture the one that is given to us
-            //    if (proxy.IsBypassed(uri)) {
-            //        originalProxy = proxy;
-            //    }
-            //    // If the current Uri is not bypassed then get a valid proxy for the Uri
-            //    // and make sure that we have the credentials also.
-            //    else {
-            //        originalProxy = new WebProxy(proxy.GetProxy(uri));
-            //        originalProxy.Credentials = proxy.Credentials == null
-            //                                        ? null : proxy.Credentials.GetCredential(uri, null);
-            //    }
-            //}
-
-            //try {
-            //    // The cached credentials that we found are not valid so let's ask the user
-            //    // until they abort or give us valid credentials.
-            //    //InitializeCredentialProxy(uri, originalProxy);
-
-            //    return PromptUserForCredentials(uri, forcePrompt: true);
-            //}
-            //finally {
-            //    // Reset the original WebRequest.DefaultWebProxy to what it was when we started credential discovery.
-            //    WebRequest.DefaultWebProxy = originalProxy;
-            //}
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2122:DoNotIndirectlyExposeMethodsWithLinkDemands")]
