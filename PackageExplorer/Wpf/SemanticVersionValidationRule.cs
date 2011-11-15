6ed235e9625e0cf9ffe0e38c0fedfba720b27c83
dotnetjunky <none@none>
2011-11-15 07:10:53 -0800

    Provider better error message when version is in incorrect format.

---
+++ PackageExplorer/Wpf/SemanticVersionValidationRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using NuGet;
+
+namespace PackageExplorer
+{
+    public class SemanticVersionValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string stringValue = (string)value;
+            if (String.IsNullOrEmpty(stringValue))
+            {
+                return ValidationResult.ValidResult;
+            }
+
+            SemanticVersion version;
+            if (SemanticVersion.TryParse(stringValue, out version))
+            {
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, "Version is in incorrect format. Examples of valid versions include '1.0', '2.0.1-alpha', '1.2.3.4-RC'.");
+            }
+        }
+    }
+}
