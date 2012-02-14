439d575d7360857bbc8234705850fb2285396457
dotnetjunky <none@none>
2012-02-14 09:13:10 -0800

    Pump version to 2.6.0.0 and change official feed to direct url.

--- Core/NuGetConstants.cs
+++ Core/NuGetConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class NuGetConstants
     {
-        public static readonly string DefaultFeedUrl = "https://go.microsoft.com/fwlink/?LinkID=230477";
+        public static readonly string DefaultFeedUrl = "https://nuget.org/api/v2/";
         public static readonly string V1FeedUrl = "https://go.microsoft.com/fwlink/?LinkID=206669";
         public static readonly string PluginFeedUrl = "http://www.myget.org/F/npe/";
     }
