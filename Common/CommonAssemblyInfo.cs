d2eca2750574c836a758892280f8ad1a554cdc53
dotnetjunky <none@none>
2012-03-10 09:15:42 -0800

    Pump version to 2.7
    --HG--
    extra : rebase_source : b07c671e0fc4f29599908d0d84dd5cfa7edcfb24

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
