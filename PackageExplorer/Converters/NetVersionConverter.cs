83783bc09e1608ddfbd0803bf18496952fab1119
dotnetjunky <dotnetjunky@gmail.com>
2013-02-13 15:48:24 -0800

    add missing files.

---
+++ PackageExplorer/Converters/NetVersionConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using NuGet;
+
+namespace PackageExplorer
+{
+    [ValueConversion(typeof(Version), typeof(string))]
+    public class NetVersionConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var version = (Version)value;
+            return version == null ? String.Empty : version.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stringValue = (string)value;
+            if (String.IsNullOrWhiteSpace(stringValue))
+            {
+                return null;
+            }
+            else
+            {
+                Version version;
+                if (Version.TryParse(stringValue, out version))
+                {
+                    return version;
+                }
+                else
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ PackageExplorer/Wpf/NetSemanticVersionValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using NuGet;
+
+namespace PackageExplorer
+{
+    public class NetSemanticVersionValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string stringValue = (string)value;
+            if (String.IsNullOrEmpty(stringValue))
+            {
+                return ValidationResult.ValidResult;
+            }
+
+            Version version;
+            if (Version.TryParse(stringValue, out version))
+            {
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, "Version is in incorrect format. Examples of valid versions include '1.0', '2.0.1', '1.2.3.4'.");
+            }
+        }
+    }
+}
