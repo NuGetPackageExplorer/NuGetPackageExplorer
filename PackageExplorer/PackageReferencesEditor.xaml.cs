d8f2f50ec5555cd0ab5ab2dd81317a6a765104db
dotnetjunky <dotnetjunky@gmail.com>
2013-04-18 09:05:19 -0700

    add reference editor dialog

---
+++ PackageExplorer/PackageReferencesEditor.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+using NuGet;
+using NuGetPackageExplorer.Types;
+using PackageExplorerViewModel;
+
+namespace PackageExplorer
+{
+    /// <summary>
+    /// Interaction logic for PackageDependencyEditor.xaml
+    /// </summary>
+    public partial class PackagReferencesEditor : StandardDialog
+    {
+        private ObservableCollection<EditablePackageDependencySet> _dependencySets = new ObservableCollection<EditablePackageDependencySet>();
+
+        private EditablePackageDependency _newPackageDependency;
+
+        public PackagReferencesEditor()
+        {
+            InitializeComponent();
+
+            DependencyGroupList.DataContext = _dependencySets;
+            ClearDependencyTextBox();
+        }
+
+        public PackagReferencesEditor(IEnumerable<PackageDependencySet> existingDependencySets)
+            : this()
+        {
+            _dependencySets.AddRange(existingDependencySets.Select(ds => new EditablePackageDependencySet(ds)));
+
+            if (_dependencySets.Count > 0)
+            {
+                DependencyGroupList.SelectedIndex = 0;
+            }
+        }
+
+        public IPackageChooser PackageChooser { get; set; }
+
+        public ICollection<PackageDependencySet> GetEditedDependencySets()
+        {
+            return _dependencySets.Select(set => set.AsReadOnly()).ToArray();
+        }
+
+        private EditablePackageDependencySet ActivePackageDependencySet
+        {
+            get
+            {
+                return (EditablePackageDependencySet)DependencyGroupList.SelectedItem;
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool canClose = String.IsNullOrEmpty(NewDependencyId.Text) || AddNewDependency();
+            if (canClose)
+            {
+                DialogResult = true;
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void RemoveDependencyButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var hyperlink = (Hyperlink)sender;
+            var selectedPackageDependency = (PackageDependency)hyperlink.DataContext;
+            if (selectedPackageDependency != null)
+            {
+                ActivePackageDependencySet.Dependencies.Remove(selectedPackageDependency);
+            }
+        }
+
+        private void SelectDependencyButtonClicked(object sender, RoutedEventArgs e)
+        {
+            PackageInfo selectedPackage = PackageChooser.SelectPackage(null);
+            if (selectedPackage != null)
+            {
+                _newPackageDependency.Id = selectedPackage.Id;
+                _newPackageDependency.VersionSpec = VersionUtility.ParseVersionSpec(selectedPackage.Version);
+            }
+        }
+
+        private void OnAddGroupClicked(object sender, RoutedEventArgs e)
+        {
+            _dependencySets.Add(new EditablePackageDependencySet());
+
+            if (DependencyGroupList.SelectedIndex == -1)
+            {
+                DependencyGroupList.SelectedIndex = _dependencySets.Count - 1;
+            }
+        }
+
+        private void OnRemoveGroupClicked(object sender, RoutedEventArgs e)
+        {
+            // remember the currently selected index;
+            int selectedIndex = DependencyGroupList.SelectedIndex;
+
+            _dependencySets.Remove((EditablePackageDependencySet)DependencyGroupList.SelectedItem);
+
+            if (_dependencySets.Count > 0)
+            {
+                // after removal, restore the previously selected index
+                selectedIndex = Math.Min(selectedIndex, _dependencySets.Count - 1);
+                DependencyGroupList.SelectedIndex = selectedIndex;
+            }
+        }
+
+        private void AddDependencyButtonClicked(object sender, RoutedEventArgs e)
+        {
+            AddNewDependency();
+        }
+
+        private bool AddNewDependency()
+        {
+            if (String.IsNullOrEmpty(NewDependencyId.Text))
+            {
+                return true;
+            }
+
+            if (!NewPackageDependencyGroup.UpdateSources())
+            {
+                return false;
+            }
+
+            ActivePackageDependencySet.Dependencies.Add(_newPackageDependency.AsReadOnly());
+
+            // after dependency is added, clear the textbox
+            ClearDependencyTextBox();
+
+            return true;
+        }
+
+        private void ClearDependencyTextBox()
+        {
+            _newPackageDependency = new EditablePackageDependency(() => ActivePackageDependencySet);
+            NewDependencyId.DataContext = _newPackageDependency;
+        }
+    }
+}
