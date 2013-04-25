fe0c11dbf234924e4c35bb0d5a06a59b784bdf66
dotnetjunky <dotnetjunky@gmail.com>
2013-04-25 12:26:28 -0700

    Update deployment url to using https

--- build.cmd
+++ build.cmd
@@ -4,6 +4,6 @@ set password=%1
 
 set deployUrl=%2
 if "%deployUrl%" == "" (
-   set deployUrl=http://nuget.codeplex.com/releases/clickonce/
+   set deployUrl=https://nuget.codeplex.com/releases/clickonce/
 )
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build\build.proj /p:Configuration=release;DeploymentUrl="%deployUrl%";Password="%password%";EnableCodeAnalysis=true
