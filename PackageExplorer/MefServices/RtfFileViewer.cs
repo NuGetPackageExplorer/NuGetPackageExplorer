898cb9d1143b62ab91ce8a24a781f9c90bd772d6
Luan Nguyen <me@yahoo.com>
2013-05-30 05:56:19 -0700

    add support for viewing .rtf files

---
+++ PackageExplorer/MefServices/RtfFileViewer.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using System.Windows.Documents;
+using NuGetPackageExplorer.Types;
+
+namespace PackageExplorer
+{
+    [PackageContentViewerMetadata(100, ".rtf")]
+    internal class RtfFileViewer : IPackageContentViewer
+    {
+        public object GetView(string extension, System.IO.Stream stream)
+        {
+            // don't display file bigger than 1MB
+            if (stream.Length > 1024 * 1024)
+            {
+                return "** This file is too big to view inline. ***";
+            }
+
+            var rtf = new RichTextBox
+            {
+                IsReadOnly = true,
+                BorderThickness = new System.Windows.Thickness(0)
+            };
+            rtf.Document.MinPageWidth = 800;
+
+            var range = new TextRange(rtf.Document.ContentStart, rtf.Document.ContentEnd);
+            range.Load(stream, System.Windows.DataFormats.Rtf);
+
+            return rtf;
+        }
+    }
+}
