0f352f733184a94c9f49c515bf0b9078c496ecd1
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-01-25 19:15:35 +0100

    verbosity:minimal build

--- build.cmd
+++ build.cmd
@@ -6,4 +6,4 @@ set deployUrl=%2
 if "%deployUrl%" == "" (
    set deployUrl=https://npe.codeplex.com/releases/clickonce/
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build\build.proj /p:Configuration=release;DeploymentUrl="%deployUrl%";Password="%password%";EnableCodeAnalysis=true
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build\build.proj  /verbosity:minimal /p:Configuration=release;DeploymentUrl="%deployUrl%";Password="%password%";EnableCodeAnalysis=true
