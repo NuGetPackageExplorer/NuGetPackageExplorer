2e9ce7029f14db68048e792295c4a166458bd9ad
Bastian Buchholz <buchholz.bastian@googlemail.com>
2016-03-30 22:43:07 +0200

    show package published date in package chooser

---
+++ PackageExplorer/Converters/DateTimeOffsetConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PackageExplorer
+{
+    public class DateTimeOffsetConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTimeOffset)
+            {
+                return ((DateTimeOffset)value).DateTime.ToLocalTime().ToString();
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
