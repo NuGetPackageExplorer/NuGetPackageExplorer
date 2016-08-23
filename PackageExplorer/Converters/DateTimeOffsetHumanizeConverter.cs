12f642f96cec422d24f7b4dd1277e2f01f895808
Jeremy Meng <yumeng@microsoft.com>
2016-08-23 09:56:50 -0700

    Pass false to utcDate parameter when humanizing LocalDateTime

--- PackageExplorer/Converters/DateTimeOffsetHumanizeConverter.cs
+++ PackageExplorer/Converters/DateTimeOffsetHumanizeConverter.cs
@@ -18,7 +18,7 @@ namespace PackageExplorer
 
                 if (dateTimeOffset != Constants.Unpublished)
                 {
-                    return dateTimeOffset.LocalDateTime.Humanize(true, null, culture);
+                    return dateTimeOffset.LocalDateTime.Humanize(false, null, culture);
                 }
             }   
             return null;
