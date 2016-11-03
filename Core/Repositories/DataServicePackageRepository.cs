2dea4a96381f755ea6c6a5f3ea5b02ca2c8b8b73
Sam <merge13@gmx.net>
2016-11-03 16:38:07 +0100

    Support listing and downloading packages when credentials are provided in the feed URL by adding a basic credential manager.

--- Core/Repositories/DataServicePackageRepository.cs
+++ Core/Repositories/DataServicePackageRepository.cs
@@ -11,16 +11,16 @@ namespace NuGet
         private readonly DataServiceContext _context;
         private DataServiceQuery<DataServicePackage> _query;
 
-        public DataServicePackageRepository(Uri uri)
-        {
-            _context = new DataServiceContext(uri);
-            _context.Credentials = CredentialCache.DefaultCredentials;
-            _context.SendingRequest += OnSendingRequest;
-            _context.IgnoreMissingProperties = true;
-            _context.Credentials = CredentialCache.DefaultCredentials;
-        }
+	    public DataServicePackageRepository(Uri uri, ICredentials credentials)
+	    {
+		    _context = new DataServiceContext(uri);
+		    _context.Credentials = credentials;
+		    _context.SendingRequest += OnSendingRequest;
+		    _context.IgnoreMissingProperties = true;
+		    _context.Credentials = credentials;
+	    }
 
-        public string Source
+	    public string Source
         {
             get { return _context.BaseUri.OriginalString; }
         }
