128eafa2af263db74dddcd47ccb0f45e016f600d
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-05-03 23:44:11 +0200

    update versions and nuspec

--- Types/Properties/AssemblyInfo.cs
+++ Types/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.12.0.0")]
-[assembly: AssemblyFileVersion("3.12.0.0")]
+[assembly: AssemblyVersion("3.21.0.0")]
+[assembly: AssemblyFileVersion("3.21.0.0")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
