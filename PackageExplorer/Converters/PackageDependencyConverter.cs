dbbdeefe9fd62d39f05fe43d987e9e7df0c4fcd9
Oren Novotny <oren@novotny.org>
2017-11-20 09:15:38 -0500

    Display dependency version number correctly

---
+++ PackageExplorer/Converters/PackageDependencyConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Data;
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
+using NuGetPe;
+
+namespace PackageExplorer
+{
+    public class PackageDependencyConverter : IValueConverter
+    {
+        VersionRangeFormatter formatter = new VersionRangeFormatter();
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            var dependency = (PackageDependency)value;
+            
+
+            var range = formatter.Format("P", dependency.VersionRange, culture);
+
+            return $"{dependency.Id} {range}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
