6384672ae4912abad5781bbf8ece1a89b66d08c5
Oren Novotny <oren@novotny.org>
2017-12-06 17:38:12 -0500

    Support multiple tokens per line

--- Core/Utility/ManifestUtility.cs
+++ Core/Utility/ManifestUtility.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -10,7 +11,10 @@ namespace NuGetPe.Utility
 {
     public static class ManifestUtility
     {
-        public const string TokenMetadata = "0.0.0+TOKEN999.";
+        public const string TokenMetadataStart = "0.0.0+TOKENSTART.";
+        public const string TokenMetadataEnd = ".TOKENEND";
+        static readonly Regex tokenRegex = new Regex(@"([$])(?:(?=(\\?))\2.)*?\1", RegexOptions.Compiled);
+        static readonly Regex metadataRegEx = new Regex(@"0\.0\.0\+TOKENSTART\.([^.]+)\.TOKENEND", RegexOptions.Compiled);
 
         public static Stream ReadManifest(string file)
         {
@@ -28,20 +32,52 @@ namespace NuGetPe.Utility
             var version = xdoc.Root.Descendants(ns + "version").FirstOrDefault();
             if (version != null)
             {
-                var val = version.Value;
-
-                // see if it's a token
-                if (val.Length >= 3 && val[0] == '$' && val[val.Length - 1] == '$')
-                {
-                    var token = val.Substring(1, val.Length - 2);
-                    version.Value = $"{TokenMetadata}{token}";
-                }
+                version.Value = ReplaceTokenWithMetadata(version.Value);
             }
 
             var ms = new MemoryStream();
             xdoc.Save(ms);
             ms.Position = 0;
             return ms;
+        }
+
+        /// <summary>
+        /// Goes from $token$ to a marker value with 0.0.0+TOKENSTART.token.TOKENEND
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ReplaceTokenWithMetadata(string value)
+        {
+            // see if it's a token
+
+            var matches = tokenRegex.Matches(value);
+            foreach (Match match in matches)
+            {
+                var token = match.Value.Substring(1, match.Value.Length - 2);
+                value = value.Replace(match.Value, $"{TokenMetadataStart}{token}{TokenMetadataEnd}");
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Goes from marker 0.0.0+TOKENSTART.token.TOKENEND to the token $token$
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ReplaceMetadataWithToken(string value)
+        {
+
+            // see if it's a token
+            var matches = metadataRegEx.Matches(value);
+
+            foreach (Match match in matches)
+            {
+                var token = match.Value.Substring(TokenMetadataStart.Length, match.Value.Length - TokenMetadataEnd.Length - TokenMetadataStart.Length);
+                value = value.Replace(match.Value, $"${token}$");
+            }
+
+            return value;
         }
 
         public static void SaveToStream(Stream sourceStream, Stream destinationStream)
@@ -55,19 +91,10 @@ namespace NuGetPe.Utility
             var version = xdoc.Root.Descendants(ns + "version").FirstOrDefault();
             if (version != null)
             {
-                var val = version.Value;
-
-                // see if it's a token
-                if (val.StartsWith(TokenMetadata))
-                {
-                    var token = val.Substring(TokenMetadata.Length);
-                    version.Value = $"${token}$";
-                }
+                version.Value = ReplaceMetadataWithToken(version.Value);
             }
 
             xdoc.Save(destinationStream);
         }
-
-        // public static 
     }
 }
