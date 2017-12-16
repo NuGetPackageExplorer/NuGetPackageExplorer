fa6566cf29bf6fbe0fe2b54c4d236aadc25e643c
Kirill Osenkov <KirillOsenkov@users.noreply.github.com>
2017-12-16 15:21:23 -0800

    Attempt to build with MSBuild 15 first, if found. (#285)
    * Attempt to build with MSBuild 15 first, if found.
    
    * Build the .sln directly.
    
    * Remove obsolete pieces.

--- build.cmd
+++ build.cmd
@@ -1,9 +1,32 @@
 @echo Off
+setlocal enableextensions enabledelayedexpansion
 
-set password=%1
-
-set deployUrl=%2
-if "%deployUrl%" == "" (
-   set deployUrl=https://npe.codeplex.com/releases/clickonce/
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
+	FOR /F "delims=" %%E in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\installer\vswhere.exe" -latest -property installationPath') DO (
+		set "MSBUILD_EXE=%%E\MSBuild\15.0\Bin\MSBuild.exe"
+		if exist "!MSBUILD_EXE!" goto :build
+	)
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build\build.proj  /verbosity:minimal /p:Configuration=release;DeploymentUrl="%deployUrl%";Password="%password%";EnableCodeAnalysis=true
+
+FOR %%E in (Enterprise, Professional, Community) DO (
+	set "MSBUILD_EXE=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%E\MSBuild\15.0\Bin\MSBuild.exe"
+	if exist "!MSBUILD_EXE!" goto :build
+)
+
+REM Couldn't be located in the standard locations, expand search
+FOR /F "delims=" %%E IN ('dir /b /ad "%ProgramFiles(x86)%\Microsoft Visual Studio\"') DO (
+	set "MSBUILD_EXE=%ProgramFiles(x86)%\Microsoft Visual Studio\%%E\MSBuild\15.0\Bin\MSBuild.exe"
+	if exist "!MSBUILD_EXE!" goto :build
+
+	FOR /F "delims=" %%F IN ('dir /b /ad "%ProgramFiles(x86)%\Microsoft Visual Studio\%%E"') DO (
+		set "MSBUILD_EXE=%ProgramFiles(x86)%\Microsoft Visual Studio\%%E\%%F\MSBuild\15.0\Bin\MSBuild.exe"
+		if exist "!MSBUILD_EXE!" goto :build
+	)
+)
+
+echo Could not find MSBuild 15, it is required to build.
+exit /b 1
+
+:build
+"%MSBUILD_EXE%" NuGetPackageExplorer.sln /t:Restore
+"%MSBUILD_EXE%" NuGetPackageExplorer.sln /m /bl /verbosity:minimal /p:Configuration=Release
