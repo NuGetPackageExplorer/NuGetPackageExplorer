ceef9df29408bdfb0405bbc7fa26809b85acc344
Khai Nguyen <khai.nguyen@outlook.com>
2018-04-16 16:36:29 -0700

    PackageDetailActionsControl: Add default ctor
    The non-existing PackageVersionsHeaderStyle is also removed from the
    GridView.

--- PackageExplorer/PackageChooser/PackageDetailActionsControl.xaml.cs
+++ PackageExplorer/PackageChooser/PackageDetailActionsControl.xaml.cs
@@ -6,10 +6,15 @@ using PackageExplorerViewModel;
 namespace PackageExplorer
 {
     /// <summary>
-    /// Interaction logic for PackageRowDetails.xaml
+    /// Interaction logic for PackageDetailActionsControl.xaml
     /// </summary>
     public partial class PackageDetailActionsControl : UserControl
     {
+        public PackageDetailActionsControl()
+        {
+            InitializeComponent();
+        }
+
         private void PackageGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var viewModel = (PackageInfoViewModel) DataContext;
