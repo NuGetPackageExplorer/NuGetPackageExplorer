2a00f6144c2212070f08beafdc23f0c7758d96e3
Oren Novotny <oren@novotny.org>
2018-04-28 11:21:08 -0400

    Use Humanizer for conversion

--- PackageExplorer/Converters/NumberToStringConverter.cs
+++ PackageExplorer/Converters/NumberToStringConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using Humanizer;
 
 namespace PackageExplorer
 {
@@ -11,35 +12,12 @@ namespace PackageExplorer
             if (value != null)
             {
                 var number = double.Parse(value.ToString());
-                return NumberToString(number, culture.NumberFormat);
+                return number.ToMetric(decimals: 1);
             }
 
             return value;
         }
-
-        private string NumberToString(double number, IFormatProvider culture)
-        {
-            var exp = 0;
-
-            while (number >= 1000)
-            {
-                number /= 1000;
-                ++exp;
-            }
-
-            var s = string.Format(culture, "{0:G3}{1}", number, ScalingFactor[exp]);
-            return s;
-        }
-
-        private static readonly string[] ScalingFactor =
-        {
-            string.Empty,
-            "K", // kilo
-            "M", // mega, million
-            "G", // giga, billion
-            "T" // tera, trillion
-        };
-
+      
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
