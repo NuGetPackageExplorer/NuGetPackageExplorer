09424002654918b16737c0edca2869fd968d06a2
campersau <buchholz.bastian@googlemail.com>
2018-04-28 14:50:09 +0200

    use SelectedPackage

--- PackageViewModel/PackageChooser/PackageChooserViewModel.cs
+++ PackageViewModel/PackageChooser/PackageChooserViewModel.cs
@@ -198,7 +198,7 @@ namespace PackageExplorerViewModel
         {
             get
             {
-                return _selectedPackageViewModel?.EffectiveSelectedPackage;
+                return _selectedPackageViewModel?.SelectedPackage;
             }
         }
         
