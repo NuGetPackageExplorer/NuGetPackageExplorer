39a9e9b818f121f749b9187b1288109678dfcb76
Oren Novotny <oren@novotny.org>
2019-06-28 10:22:58 -0400

    supressions

--- PackageExplorer/MefServices/UIServices.cs
+++ PackageExplorer/MefServices/UIServices.cs
@@ -134,25 +134,16 @@ namespace PackageExplorer
 
         public void Show(string message, MessageLevel messageLevel)
         {
-            MessageBoxImage image;
-            switch (messageLevel)
+            var image = messageLevel switch
             {
-                case MessageLevel.Error:
-                    image = MessageBoxImage.Error;
-                    break;
+                MessageLevel.Error => MessageBoxImage.Error,
 
-                case MessageLevel.Information:
-                    image = MessageBoxImage.Information;
-                    break;
+                MessageLevel.Information => MessageBoxImage.Information,
 
-                case MessageLevel.Warning:
-                    image = MessageBoxImage.Warning;
-                    break;
+                MessageLevel.Warning => MessageBoxImage.Warning,
 
-                default:
-                    throw new ArgumentOutOfRangeException("messageLevel");
-            }
-
+                _ => throw new ArgumentOutOfRangeException("messageLevel"),
+            };
             void ShowDialog()
             {
                 MessageBox.Show(
