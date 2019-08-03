0a97857023e6a5ba6c885d72f48cad6d15f3b8ff
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-08-03 18:43:19 +0200

    Added client search packages
    Including prelease filter, id filter, paging and sorting

--- PackageViewModel/PackageChooser/ShowLatestVersionQueryContext.cs
+++ PackageViewModel/PackageChooser/ShowLatestVersionQueryContext.cs
@@ -1,31 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using NuGet.Common;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
+using PackageExplorerViewModel.PackageSearch;
 
 namespace PackageExplorerViewModel
 {
     internal class ShowLatestVersionQueryContext<T> : IQueryContext<T> where T : IPackageSearchMetadata
     {
         private readonly SourceRepository _sourceRepository;
-        private readonly string? _searchText;
-        private readonly SearchFilter _searchFilter;
+        private readonly SearchContext _searchContext;
         private readonly int _pageSize;
         private PackageSearchResource? _packageSearchResouce;
         private RawSearchResourceV3? _rawPackageSearchResouce;
         private int? _lastPageIndex;
         private int _lastPageCount;
+        private readonly PackageListCache<T> _packageListCache;
+        private readonly LocalPackageSearcher<T> _localPackageSearcher;
 
-        public ShowLatestVersionQueryContext(SourceRepository sourceRepository, string? search, bool showPreReleasePackages, int pageSize)
+        public ShowLatestVersionQueryContext(SourceRepository sourceRepository, string? search, bool showPreReleasePackages, int pageSize, PackageListCache<T> packageListCache)
         {
             _sourceRepository = sourceRepository;
-            _searchText = search;
-            _searchFilter = new SearchFilter(showPreReleasePackages);
+            _searchContext = new SearchContext(search, new SearchFilter(showPreReleasePackages));
             _pageSize = pageSize;
+            _packageListCache = packageListCache ?? throw new ArgumentNullException(nameof(packageListCache));
+            _localPackageSearcher = new LocalPackageSearcher<T>(_searchContext);
         }
 
         #region IQueryContext<T> Members
@@ -40,16 +44,70 @@ namespace PackageExplorerViewModel
 
         public async Task<IList<T>> GetItemsForCurrentPage(CancellationToken token)
         {
-            IEnumerable<IPackageSearchMetadata>? result = null;
-
-            if (_searchText?.StartsWith("id:", StringComparison.OrdinalIgnoreCase) == true)
+            var packageSource = _sourceRepository.PackageSource.Source;
+            List<T> list;
+            if (_packageListCache.TryGetPackages(packageSource, out var packages))
             {
-                var id = _searchText?.Substring(3).Trim();
-                if (!string.IsNullOrEmpty(id))
-                {
-                    var findPackageByIdResource = await _sourceRepository.GetResourceAsync<PackageMetadataResource>();
+                list = LocalSearch(packages);
+            }
+            else
+            {
+                list = await SearchOnServer(token);
 
-                    var metadata = await findPackageByIdResource.GetMetadataAsync(id, _searchFilter.IncludePrerelease, true, NullSourceCacheContext.Instance, NullLogger.Instance, token);
+                if (list.Count > _pageSize)
+                {
+                    // More packages than requested, assume static feed
+                    _packageListCache.SetPackages(packageSource, list);
+                    list = LocalSearch(list);
+                }
+            }
+
+            if (list.Count < _pageSize)
+            {
+                _lastPageIndex = CurrentPage;
+                _lastPageCount = list.Count;
+            }
+
+            return list;
+        }
+
+        private List<T> LocalSearch(IEnumerable<T> packages)
+        {
+            var items = _localPackageSearcher.SearchPackages(packages);
+
+            var list = ApplyPaging(items, _pageSize);
+            return list;
+        }
+
+        private List<T> ApplyPaging(IEnumerable<T> packages, int pageSize)
+        {
+            var packagesList = packages.ToList();
+            if (packagesList.Count > pageSize)
+            {
+                var skip = BeginPackage - 1;
+                var count = EndPackage - skip;
+
+                if (packagesList.Count > count + skip)
+                {
+                    // more packages then needed.
+                    packagesList = packagesList.Skip(skip).Take(count).ToList();
+                }
+            }
+
+            return packagesList;
+        }
+
+        private async Task<List<T>> SearchOnServer(CancellationToken token)
+        {
+            var searchText = _searchContext.SearchText;
+            IEnumerable<IPackageSearchMetadata>? result = null;
+            if (_searchContext.IsIdSearch)
+            {
+                if (!string.IsNullOrEmpty(searchText))
+                {
+                    var findPackageByIdResource = await _sourceRepository.GetResourceAsync<PackageMetadataResource>(token);
+
+                    var metadata = await findPackageByIdResource.GetMetadataAsync(searchText, _searchContext.Filter.IncludePrerelease, true, NullSourceCacheContext.Instance, NullLogger.Instance, token);
 
                     result = metadata.OrderByDescending(m => m.Identity.Version).Take(1);
                 }
@@ -64,9 +122,10 @@ namespace PackageExplorerViewModel
                 {
                     _rawPackageSearchResouce = await _sourceRepository.GetResourceAsync<RawSearchResourceV3>(token);
                 }
+                
                 if (_rawPackageSearchResouce != null)
                 {
-                    var json = await _rawPackageSearchResouce.Search(_searchText, _searchFilter, CurrentPage * _pageSize, _pageSize, NullLogger.Instance, token);
+                    var json = await _rawPackageSearchResouce.Search(searchText, _searchContext.Filter, CurrentPage * _pageSize, _pageSize, NullLogger.Instance, token);
 
                     result = json.Select(s => s.FromJToken<PackageSearchMetadata>());
                 }
@@ -78,20 +137,13 @@ namespace PackageExplorerViewModel
                         _packageSearchResouce = await _sourceRepository.GetResourceAsync<PackageSearchResource>(token);
                     }
 
-                    result = await _packageSearchResouce.SearchAsync(_searchText, _searchFilter, CurrentPage * _pageSize, _pageSize, NullLogger.Instance, token);
+                    result = await _packageSearchResouce.SearchAsync(searchText, _searchContext.Filter, CurrentPage * _pageSize, _pageSize, NullLogger.Instance, token);
                 }
             }
 
             token.ThrowIfCancellationRequested();
 
             var list = result.Cast<T>().ToList();
-
-            if (list.Count < _pageSize)
-            {
-                _lastPageIndex = CurrentPage;
-                _lastPageCount = list.Count;
-            }
-
             return list;
         }
 
---
+++ PackageViewModel/PackageSearch/PackageListCache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using NuGet.Protocol.Core.Types;
+
+namespace PackageExplorerViewModel.PackageSearch
+{
+    internal class PackageListCache<T>  where T : IPackageSearchMetadata
+    {
+        private readonly Dictionary<string, List<T>> _packagesDict = new Dictionary<string, List<T>>(StringComparer.OrdinalIgnoreCase);
+
+        public void SetPackages(string packageSource, List<T> packages)
+        {
+            _packagesDict[packageSource] = packages;
+        }
+
+        public bool TryGetPackages(string packageSource, out List<T> packages)
+        {
+            return _packagesDict.TryGetValue(packageSource, out packages);
+        }
+    }
+}
