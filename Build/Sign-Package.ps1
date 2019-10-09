6c84c8593abab3e08da2771381bd8025a6f69261
Oren Novotny <oren@novotny.org>
2019-10-09 10:42:15 -0400

    use quotes around parameters

--- Build/Sign-Package.ps1
+++ Build/Sign-Package.ps1
@@ -10,7 +10,7 @@ if([string]::IsNullOrEmpty($Env:SignClientSecret)){
 $appSettings = "$currentDirectory\appsettings.json"
 $fileList = "$currentDirectory\filelist.txt"
 
-& $currentDirectory\SignClient 'sign' -c $appSettings -b $Env:ArtifactDirectory -i **/*.{appxbundle,appinstaller,zip,nupkg} -f $fileList -r $Env:SignClientUser -s $Env:SignClientSecret -n 'NuGet Package Explorer' -d 'NuGet Package Explorer' -u 'https://github.com/NuGetPackageExplorer/NuGetPackageExplorer'
+& $currentDirectory\SignClient "sign" -c "$appSettings" -b "$Env:ArtifactDirectory" -i "**/*.{appxbundle,appinstaller,zip,nupkg}" -f "$fileList" -r $Env:SignClientUser -s $Env:SignClientSecret -n "NuGet Package Explorer" -d "NuGet Package Explorer" -u "https://github.com/NuGetPackageExplorer/NuGetPackageExplorer"
 
 if ($LASTEXITCODE -ne 0) {
   exit 1
