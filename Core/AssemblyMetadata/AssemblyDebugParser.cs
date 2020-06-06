3bbcfd76dd25d3765062c4aef682b8e355a8d9c4
Claire Novotny <claire@novotny.org>
2020-06-06 12:08:58 -0400

    Retrieve compiler flags and metadata reference data as defined in https://github.com/dotnet/roslyn/pull/44373

--- Core/AssemblyMetadata/AssemblyDebugParser.cs
+++ Core/AssemblyMetadata/AssemblyDebugParser.cs
@@ -80,8 +80,11 @@ namespace NuGetPe.AssemblyMetadata
         private readonly byte[]? _peBytes;
 
         private static readonly Guid SourceLinkId = new Guid("CC110556-A091-4D38-9FEC-25AB9A351A6A");
-
         private static readonly Guid EmbeddedSourceId = new Guid("0E8A571B-6926-466E-B4AD-8AB04611F5FE");
+        private static readonly Guid CompilerFlagsId = new Guid("B5FEEC05-8CD0-4A83-96DA-466284BB4BD8");
+        private static readonly Guid MetadataReferencesId = new Guid("7E4D4708-096E-4C5C-AEDA-CB10BA6A740D");
+
+
         private const ushort PortableCodeViewVersionMagic = 0x504d;
 
 
@@ -98,6 +101,8 @@ namespace NuGetPe.AssemblyMetadata
                 SourceLinkErrors = errors,
                 SymbolKeys = GetSymbolKeys(_peReader),
                 PdbChecksumIsValid = VerifyPdbChecksums(),
+                CompilerFlags = GetCompilerFlags(),
+                MetadataReferences = GetMetadataReferences(),
                 HasDebugInfo = true
             };
 
@@ -118,6 +123,110 @@ namespace NuGetPe.AssemblyMetadata
             if (blobh.IsNil) return Array.Empty<byte>();
             return _reader.GetBlobBytes(blobh);
         }
+
+        private IReadOnlyCollection<CompilerFlag> GetCompilerFlags()
+        {
+            var flags = new List<CompilerFlag>();
+
+            foreach (var cdih in _reader.GetCustomDebugInformation(EntityHandle.ModuleDefinition))
+            {
+                var customDebugInformation = _reader.GetCustomDebugInformation(cdih);
+                if (_reader.GetGuid(customDebugInformation.Kind) == CompilerFlagsId)
+                {
+                    var blobReader = _reader.GetBlobReader(customDebugInformation.Value);
+
+                    // Compiler flag bytes are UTF-8 null-terminated key-value pairs
+                    var nullIndex = blobReader.IndexOf(0);
+                    while (nullIndex >= 0)
+                    {
+                        var key = blobReader.ReadUTF8(nullIndex);
+
+                        // Skip the null terminator
+                        blobReader.ReadByte();
+
+                        nullIndex = blobReader.IndexOf(0);
+                        var value = blobReader.ReadUTF8(nullIndex);
+
+                        // Skip the null terminator
+                        blobReader.ReadByte();
+
+                        nullIndex = blobReader.IndexOf(0);
+
+                        // key and value now have strings containing serialized compiler flag information
+                        flags.Add(new CompilerFlag { Key = key, Value = value });
+                    }
+                }
+            }
+
+            return flags;
+        }
+
+        private IReadOnlyCollection<MetadataReference> GetMetadataReferences()
+        {
+            var references = new List<MetadataReference>();
+
+            foreach (var cdih in _reader.GetCustomDebugInformation(EntityHandle.ModuleDefinition))
+            {
+                var customDebugInformation = _reader.GetCustomDebugInformation(cdih);
+                if (_reader.GetGuid(customDebugInformation.Kind) == MetadataReferencesId)
+                {
+                    var blobReader = _reader.GetBlobReader(customDebugInformation.Value);
+
+                    // Each loop is one reference
+                    while (blobReader.RemainingBytes > 0)
+                    {
+                        // Order of information
+                        // File name (null terminated string): A.exe
+                        // Extern Alias (null terminated string): a1,a2,a3
+                        // EmbedInteropTypes/MetadataImageKind (byte)
+                        // COFF header Timestamp field (4 byte int)
+                        // COFF header SizeOfImage field (4 byte int)
+                        // MVID (Guid, 24 bytes)
+
+                        var terminatorIndex = blobReader.IndexOf(0);
+
+                        var name = blobReader.ReadUTF8(terminatorIndex);
+
+                        // Skip the null terminator
+                        blobReader.ReadByte();
+
+                        terminatorIndex = blobReader.IndexOf(0);
+
+                        var externAliases = blobReader.ReadUTF8(terminatorIndex);
+
+                        // Skip the null terminator
+                        blobReader.ReadByte();
+
+                        var embedInteropTypesAndKind = blobReader.ReadByte();
+                        var embedInteropTypes = (embedInteropTypesAndKind & 0b10) == 0b10;
+                        var kind = (embedInteropTypesAndKind & 0b1) == 0b1
+                            ? MetadataImageKind.Assembly
+                            : MetadataImageKind.Module;
+
+                        // seconds since 00:00 UTC Jan 1 1970 (Unix epoch)
+                        var timestamp = blobReader.ReadInt32();
+                        var imageSize = blobReader.ReadInt32();
+                        var mvid = blobReader.ReadGuid();
+
+                        references.Add(new MetadataReference
+                        {
+                            Name = name,
+                            ExternAliases = externAliases,
+                            EmbedInteropTypes = embedInteropTypes,
+                            MetadataImageKind = kind,
+                            Timestamp = DateTimeOffset.FromUnixTimeSeconds(timestamp),
+                            ImageSize = imageSize,
+                            Mvid = mvid
+                        });
+
+                    }
+                }
+
+            }
+            return references;
+        }
+
+
 
         private bool IsEmbedded(DocumentHandle dh)
         {
