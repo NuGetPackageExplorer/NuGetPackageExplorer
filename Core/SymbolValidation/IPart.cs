069c91023c3d8aa2c6a10ff0b4c2551034e2d70f
Cédric Luthi <cedric.luthi@gmail.com>
2020-11-24 21:40:32 +0100

    Move SymbolValidator to the Core project

---
+++ Core/SymbolValidation/IPart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace NuGetPe
+{
+    public interface IPart
+    {
+        string Path { get; }
+
+        string Name { get; }
+
+        IFolder? Parent { get; }
+
+        IEnumerable<IFile> GetFiles();
+    }
+}
