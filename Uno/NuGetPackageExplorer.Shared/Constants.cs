8d4319b3ab14db5b46ad422d9bf3d0984d9d6669
xiaoy312 <xiaoyao312@gmail.com>
2021-08-06 14:37:30 -0400

    fix: app crash on skia.gtk with debug build

--- Uno/NuGetPackageExplorer.Shared/Constants.cs
+++ Uno/NuGetPackageExplorer.Shared/Constants.cs
@@ -6,10 +6,6 @@ namespace NuGetPackageExplorer
 {
     public static class Constants
     {
-#if DEBUG
-        public const string LocalTestPackagePath = "./tmp/mynuget.nupkg";
-#endif
-
         public const string AppName = "NuGet Package Explorer";
 
         public const string NuGetOrgSource = "https://api.nuget.org/v3/index.json";
--- Uno/NuGetPackageExplorer.Skia.Gtk/Program.cs
+++ Uno/NuGetPackageExplorer.Skia.Gtk/Program.cs
@@ -17,27 +17,9 @@ namespace PackageExplorer
 				expArgs.ExitApplication = true;
 			};
 
-#if DEBUG
-            ExtractTestPackage();
-#endif
-
 			var host = new GtkHost(() => new App(), args);
 
 			host.Run();
 		}
-
-#if DEBUG // TODO: remove debug code
-        private static void ExtractTestPackage()
-        {
-            var names = typeof(App).Assembly.GetManifestResourceNames();
-            var name = names.FirstOrDefault(n => n.EndsWith(".nupkg")) ?? throw new FileNotFoundException("test package not found in manifest resources");
-
-            Directory.CreateDirectory(Path.GetDirectoryName(NuGetPackageExplorer.Constants.LocalTestPackagePath));
-            using (var file = File.OpenWrite(NuGetPackageExplorer.Constants.LocalTestPackagePath))
-            {
-                typeof(App).Assembly.GetManifestResourceStream(name).CopyTo(file);
-            }
-        }
-#endif
     }
 }
