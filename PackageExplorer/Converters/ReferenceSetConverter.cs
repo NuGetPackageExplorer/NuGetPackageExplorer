bdd4115599f12ea494c64fb490ed3f89934caac4
Luke Blevins <lukeblevins15@outlook.com>
2023-03-16 17:30:26 -0400

    fix: fix null exception in converters

--- PackageExplorer/Converters/ReferenceSetConverter.cs
+++ PackageExplorer/Converters/ReferenceSetConverter.cs
@@ -19,16 +19,22 @@ namespace PackageExplorer
     {
         public object Convert(object value, Type targetType, object parameter, _CultureInfo culture)
         {
-            var referenceSets = (ICollection<PackageReferenceSet>)value;
-            if (referenceSets.Any(d => d.TargetFramework != null))
+            if (value is ICollection<PackageReferenceSet> referenceSets)
             {
-                // if there is at least one dependeny set with non-null target framework,
-                // we show the dependencies grouped by target framework.
-                return referenceSets;
-            }
+                if (referenceSets.Any(d => d.TargetFramework != null))
+                {
+                    // if there is at least one dependeny set with non-null target framework,
+                    // we show the dependencies grouped by target framework.
+                    return referenceSets;
+                }
 
-            // otherwise, flatten the groups into a single list of dependencies
-            return referenceSets.SelectMany(d => d.References);
+                // otherwise, flatten the groups into a single list of dependencies
+                return referenceSets.SelectMany(d => d.References);
+            }
+            else
+            {
+                return new Enumerable.Empty<PackageReferenceSet>();
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, _CultureInfo culture)
