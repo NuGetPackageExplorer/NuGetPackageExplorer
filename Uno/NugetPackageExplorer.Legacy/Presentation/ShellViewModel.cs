b68bb27999e8dbedb678bcc8777069273d90bb96
Luke Blevins <lukeblevins15@outlook.com>
2023-03-28 08:27:07 -0400

    feat: re-evaluate back button load state

--- Uno/NugetPackageExplorer.Legacy/Presentation/ShellViewModel.cs
+++ Uno/NugetPackageExplorer.Legacy/Presentation/ShellViewModel.cs
@@ -6,9 +6,12 @@ using System.Windows.Input;
 
 using Microsoft.Extensions.DependencyInjection;
 using Uno.Foundation;
+using Windows.Foundation;
+using Windows.UI.Xaml;
 using NupkgExplorer.Framework.MVVM;
 using NupkgExplorer.Framework.Navigation;
 using NupkgExplorer.Presentation.Content;
+using System.Reflection.Emit;
 
 namespace NupkgExplorer.Presentation
 {
@@ -22,14 +25,36 @@ namespace NupkgExplorer.Presentation
             set => SetProperty(value);
         }
 
+        public Size WindowSize
+        {
+            get => GetProperty<Size>();
+            set => SetProperty(value);
+        }
+
         public ShellViewModel()
         {
 
 #if __WASM__
-            IsStandaloneWindow = WebAssemblyRuntime.InvokeJS(@"window.matchMedia('(display-mode: standalone)').matches") == "true";
+            WindowSize = new Size(Window.Current.Bounds.Width, Window.Current.Bounds.Height);
+            IsStandaloneWindow = IsWindowStandalone();
+            Window.Current.SizeChanged += (s, e) =>
+            {
+                var newSize = new Size(e.Size.Width, e.Size.Height);
+
+                if (Math.Abs(newSize.Width - WindowSize.Width) > 10 || Math.Abs(newSize.Height - WindowSize.Height) > 10)
+                {
+                    IsStandaloneWindow = IsWindowStandalone();
+                }
+                WindowSize = newSize;
+            };
 #else
             IsStandaloneWindow = true;
 #endif
+        }
+
+        private bool IsWindowStandalone()
+        {
+            return WebAssemblyRuntime.InvokeJS(@"window.matchMedia('(display-mode: standalone)').matches") == "true";
         }
     }
 }
