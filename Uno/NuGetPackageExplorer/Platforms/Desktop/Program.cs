578a8a52052d230abecc7568be358b442761edaa
Jerome Laban <jerome@platform.uno>
2025-06-19 16:00:15 -0400

    chore: Bump to uno 6

--- Uno/NuGetPackageExplorer/Platforms/Desktop/Program.cs
+++ Uno/NuGetPackageExplorer/Platforms/Desktop/Program.cs
@@ -1,4 +1,5 @@
-﻿using Uno.UI.Runtime.Skia;
+﻿using Uno.UI.Hosting;
+using Uno.UI.Runtime.Skia;
 
 namespace PackageExplorer;
 
@@ -9,7 +10,7 @@ public class Program
     {
         App.InitializeLogging();
 
-        var host = SkiaHostBuilder.Create()
+        var host = UnoPlatformHostBuilder.Create()
             .App(() => new App())
             .UseX11()
             .UseLinuxFrameBuffer()
