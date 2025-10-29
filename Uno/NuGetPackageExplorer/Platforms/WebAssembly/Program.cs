5712825896df6dcbbc45f02ca6fb53dc94b46f83
Claire Novotny <claire@novotny.org>
2025-10-29 12:38:17 -0400

    Refactor Program.cs for Uno Platform updates
    Removed unused imports and obsolete P/Invoke workaround.
    Added Uno.UI.Hosting for WebAssembly support.
    Commented out unneeded API logging configuration.

--- Uno/NuGetPackageExplorer/Platforms/WebAssembly/Program.cs
+++ Uno/NuGetPackageExplorer/Platforms/WebAssembly/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices;
-
-using Uno.UI.Hosting;
+﻿using Uno.UI.Hosting;
 
 namespace PackageExplorer
 {
@@ -8,7 +6,7 @@ namespace PackageExplorer
     {
         public static async Task Main(string[] args)
         {
-            FeatureConfiguration.ApiInformation.NotImplementedLogLevel = Uno.Foundation.Logging.LogLevel.Debug;
+            //FeatureConfiguration.ApiInformation.NotImplementedLogLevel = Uno.Foundation.Logging.LogLevel.Debug;
 
             var host = UnoPlatformHostBuilder.Create()
                 .App(() => new App())
@@ -17,11 +15,5 @@ namespace PackageExplorer
 
             await host.RunAsync();
         }
-
-        // In net8, calls to chmod for a separate library are causing issues
-        // https://github.com/NuGet/NuGet.Client/blob/824a3c7d8823c3be1cf48e08e5f1993d2e8eb4ab/src/NuGet.Core/NuGet.Configuration/Utility/FileSystemUtility.cs#L53
-        // Declaring the import in the main assembly help getting the p/invoke call resolved.
-        [DllImport("libc.so", SetLastError = true)]
-        private static extern int chmod(string pathname, int mode);
     }
 }
